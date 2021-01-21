d705791a588daac2d9020d41a945637ceeb0f66c
Bruce Ricketts <byricketts98@gmail.com>
2021-01-21 17:09:08 -0500

    Fix index export

---
+++ index.ts
@@ -0,0 +1,24 @@
+export const defaultAlertSettings = {
+  specVersion: 1,
+  pollingInterval: 'ONE_DAY',
+  outputs: ['queries.query0.total', 'alertLevel'],
+  operations: [
+    {
+      when: {
+        type: 'FILTER',
+        specVersion: 1,
+        condition: '{{queries.query0.total > 0}}',
+      },
+      actions: [
+        {
+          type: 'SET_PROPERTY',
+          targetProperty: 'alertLevel',
+          targetValue: 'HIGH',
+        },
+        {
+          type: 'CREATE_ALERT',
+        },
+      ],
+    },
+  ],
+};
