fc9f7d1db6932aba4310d51818c1fb6d37eaf278
Jayson Jensen <jayson.jensen@jupiterone.com>
2021-03-26 17:34:28 -0500

    Add the start of engame rules

--- rule-packs/index.js
+++ rule-packs/index.js
@@ -1,5 +1,6 @@
 module.exports.AwsConfig= require('./aws-config.json');
 module.exports.AwsThreat= require('./aws-threat.json');
+module.exports.AwsEngame= require('./aws-endgame.json');
 module.exports.AzureConfig= require('./azure-config.json');
 module.exports.GCP = require('./gcp.json');
 module.exports.CommonAlerts= require('./common-alerts.json');
